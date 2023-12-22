313079e445bfc3352083213d71b8e408d16a2814
SwagatKar <s.kar.swagat@gmail.com>
2023-12-22 17:04:30 +0530

    pwa issue solved

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'push-notification'`, () => {
+  it(`should have as title 'push-notification-pwa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('push-notification');
+    expect(app.title).toEqual('push-notification-pwa');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('push-notification app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('push-notification-pwa app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ initializeApp(environment.firebase);
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      enabled: true,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
